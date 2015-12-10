6b8b4f9508a956f51ca158f2381e0fa18636b6db
abogdan0505 <abogdan0505@gmail.com>
2015-12-10 23:35:11 +0200

    first commit

---
+++ app/scripts/controllers/TabularCtrl.js
@@ -0,0 +1,13 @@
+;(function (angular) {
+    'use strict';
+
+    angular
+        .module('controllers.Tabular', [])
+        .controller('TabularCtrl', TabularCtrl);
+
+    TabularCtrl.$inject = [];
+
+    function TabularCtrl () {}
+
+})(angular);
+
